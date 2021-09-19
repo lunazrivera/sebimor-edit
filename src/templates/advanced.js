58d94e96f41f67e0e41c5198834465c9df77bd81
Stackbit Code Editor <projects@stackbit.com>
2021-09-19 23:02:15 +0000

    advanced.js: changed

--- src/templates/advanced.js
+++ src/templates/advanced.js
@@ -18,11 +18,11 @@ export default class Advanced extends React.Component {
     render() {
         return (
             <Layout {...this.props}>
-            {(!_.get(this.props, 'pageContext.frontmatter.hide_title', null)) && (
+            {/* {(!_.get(this.props, 'pageContext.frontmatter.hide_title', null)) && (
             <header className="page-header inner-sm outer">
               <h1 className="page-title line-top">{_.get(this.props, 'pageContext.frontmatter.title', null)}</h1>
             </header>
-            )}
+            )} */}
             {_.map(_.get(this.props, 'pageContext.frontmatter.sections', null), (section, section_idx) => {
                 let component = _.upperFirst(_.camelCase(_.get(section, 'type', null)));
                 let Component = components[component];
